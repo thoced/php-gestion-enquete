e9691c9f4690fe06143f6dce596ccc784d6aa102
Thonon <Thonon@Thonon-PC.lan>
2017-07-24 19:51:41 +0200

    - Creation du ViewFoldersView

---
+++ App/Folder/ViewFoldersView.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "view folders view";
+
+?>
+
+<a href="?target_link=" >Retour</a>
+
