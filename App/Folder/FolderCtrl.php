deef3fe8bdc461a87286f0b9cf1897cf68c7defa
Thonon <Thonon@Thonon-PC.lan>
2017-07-24 19:49:16 +0200

    - suppression de la class FolderCtrl

--- App/Folder/FolderCtrl.php
+++ App/Folder/FolderCtrl.php
@@ -1,46 +1,4 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require 'ViewFoldersView.php';
 
-/**
- * Description of FolderCtrl
- *
- * @author Thonon
- */
-class FolderCtrl implements IController
-{
-    private static $_instance;
-    
-    private $db;
-    
-    private function __construct() {
-        
-        $this->db = DbConnect::getInstance();
-    }
-    
-    public function run() 
-    {
-        
-    }
-    
-    public function showAllFolders()
-    {
-        
-    }
-    
-    public static function getInstance()
-    {
-        if(self::$_instance !== null)
-        {
-            self::$_instance = new FolderCtrl();
-            return self::$_instance;
-        }
-        else
-            return self::$_instance;
-    }
-
-}
