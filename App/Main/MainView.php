bfa32acfddf4e209ee5b987409df8357afeefb05
Thonon <Thonon@Thonon-PC.lan>
2017-07-25 14:48:43 +0200

    - Ajout gestion des personnes

--- App/Main/MainView.php
+++ App/Main/MainView.php
@@ -37,20 +37,23 @@
     <p id="Title">Gestion Enquête Locale</p>
     <p><a href="?target_link=LOGOUT">Logout</a></p>
     <div id="login">Utilisateur :
-      <?php echo $user;?> <br> Dossier en cours: 
-      <?php echo $folder;?>
-    </div>
+      <?php echo $user;?> <br>
+      Dossier en cours:
+      <?php echo $folder;?> </div>
     <p><a href="?target_link=LOGOUT"></a><br>
     </p>
     <br>
+    <p><br>
+    </p>
     <table style="width: 100%" border="1">
       <tbody>
         <tr>
-          <td style="width: 153.967px;">Gestion des Dossiers<br>
+          <td style="width: 180.917px;">Gestion des Dossiers<br>
             <br>
             <a href="?target_link=VIEWDOSSIERS">Voir les dossiers</a></td>
-          <td style="width: 739.35px;"><br>
-          </td>
+          <td style="width: 404.7px;">Gestion des Personnes physique<br>
+            <br>
+            <a href="?target_link=VIEWPERSONNES">Voir les personnes</a></td>
           <td><br>
           </td>
           <td><br>
