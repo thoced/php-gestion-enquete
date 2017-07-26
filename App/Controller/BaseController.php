2b7d1fc3daeb4e371d7cbc07b0e5acd9bcaf6e7b
Thonon <Thonon@Thonon-PC.lan>
2017-07-26 09:50:15 +0200

    - Creation d'un controller abstract servant de pompe à message des actions à effectuer ex: DELETE,UPDATE

---
+++ App/Controller/BaseController.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BaseController
+ *
+ * @author Thonon
+ */
+abstract class BaseController 
+{
+    abstract public function run($setting,$action,$id);
+    
+    public function __construct() 
+    {
+        // pompe à récupération d'information
+        $setting = new SettingModel();
+        $action = null;
+        $id = null;
+        
+        if(isset($_SESSION['SETTING'])){
+            $setting->unserialize($_SESSION['SETTING']);
+        }
+        
+        if(isset($_GET['action'])){
+            $action = $_GET['action'];
+        }
+        
+        if(isset($_GET['id'])){
+            $id = $_GET['id'];
+        }
+        
+        // appel au methode run des enfants
+        $this->run($setting,$action,$id);
+    }
+}
