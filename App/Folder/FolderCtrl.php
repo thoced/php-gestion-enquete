4d81686bc84104a9e6a9973bf9de80f5c1ccd215
Thonon <Thonon@Thonon-PC.lan>
2017-07-26 16:00:08 +0200

    - Création de la classe FolderCtrl qui dérive de BaseController - cette classe gère la gestion des Folders

--- App/Folder/FolderCtrl.php
+++ App/Folder/FolderCtrl.php
@@ -1,44 +1,55 @@
 <?php
 
-require_once './App/Folder/FolderModel.php';
-require_once './App/Setting/SettingModel.php';
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-// reception de l'objet login
-if(!isset($_SESSION))
-    session_start();
+/**
+ * Description of FolderCtrl
+ *
+ * @author Thonon
+ */
+require_once './App/Controller/BaseController.php';
 
-$login = new LoginModel('','');
-$login->unserialize($_SESSION['LOGIN']);
 
-// reception de la liste des folders
-$folderModel = new FolderModel();
-$listFolders = $folderModel->getFolders($login);
-// code de sélection
-
-if(isset($_GET['IDSELECT']))
-{
-    $idselect = $_GET['IDSELECT'];
-    if(is_numeric($idselect))
-    {
-        if(isset($_SESSION['SETTING'])){
-              
-             $setting = new SettingModel();
-             $setting->unserialize($_SESSION['SETTING']);
-             $setting->setIdFolderSelected($idselect);
-             $_SESSION['SETTING'] = $setting->serialize();
-        }
-        else
+class FolderCtrl extends BaseController{
+    //put your code here
+    public function run($login,$setting, $action, $id, $update) {
+        
+        switch($action)
         {
-              
-            $setting = new SettingModel();
-            $setting->setIdFolderSelected($idselect);
-            $_SESSION['SETTING'] = $setting->serialize();
+            case 'SELECT':$this->selectFolder($setting,$id);
+                break;
+            
+            default: $this->showFolder($login);
+                     break;
         }
-     
     }
-    header('location: .?target_link=MAINVIEW');
-    die;
+    
+    private function showFolder($login)
+    {
+        $db = DbConnect::getInstance();
+        $req = $db->_dbb->prepare("select * from t_folders inner join t_link_group_folders on t_folders.id = t_link_group_folders.ref_id_folders "
+                    . "where t_link_group_folders.ref_id_group like ("
+                    . "select DISTINCT t_group.id from t_group inner join t_link_group_users on t_group.id = t_link_group_users.ref_id_group "
+                    . "where t_link_group_users.ref_id_users = (select t_users.id from t_users where login = :login)) AND t_folders.visible = TRUE");
+        
+        $req->execute(array("login" => $login->login));
+        
+        // appel à la vue
+        require './App/Folder/ViewFoldersView.php';
+    }
+    
+    private function selectFolder($setting,$id)
+    {
+        if(is_numeric($id)){
+            $setting->setIdFolderSelected($id);
+            $_SESSION['SETTING'] = $setting->serialize();
+            header('location: .?target_link=MAINVIEW');
+            
+        }
+    }
+
 }
-
-require 'ViewFoldersView.php';
-
