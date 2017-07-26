ac721969a23dbf645e362e21b5c646b53f58668a
Thonon <Thonon@Thonon-PC.lan>
2017-07-26 09:50:57 +0200

    - La classe dérive maintenant de BaseController et récupère via la methode Run le Setting, action et id

--- App/Personne/PersonneCtrl.php
+++ App/Personne/PersonneCtrl.php
@@ -11,28 +11,53 @@
  *
  * @author Thonon
  */
-class PersonneCtrl 
+require_once './App/Controller/BaseController.php';
+
+class PersonneCtrl extends BaseController
 {
-    private $setting;
     
-    private function run()
+    public function run($setting,$action,$id)
     {
+ 
+        switch($action)
+        {
+            case 'DELETE':echo "DELETE";$this->deletePerson($setting, $id);
+                          break;
+            
+            default:$this->showPerson($setting);
+                    break;
+        }
+
+    }
+    
+    private function deletePerson($setting,$id)
+    {
+         // suppresion d'une personne
+       
+        if(is_numeric($id)){
+        $db = DbConnect::getInstance();
+        $req = $db->_dbb->prepare('delete from t_personne where id = :id');
+        $req->execute(array('id' =>$id));
+        }
         
-        
-        
-        // reception des Personnes
+        $this->showPerson($setting);
+    }
+    
+    private function showPerson($setting)
+    {
+         // reception des Personnes
         $db = DbConnect::getInstance();
         $req = $db->_dbb->prepare('select * from t_personne where ref_id_folders = :refFolder');
-        $req->execute(array('refFolder' => $this->setting->getIdFolderSelected()));
-               
+        $req->execute(array('refFolder' => $setting->getIdFolderSelected()));
+        
         // appel à la vue
         require './App/Personne/PersonneView.php';
     }
     
-    function __construct($setting) {
-        $this->setting = $setting;
-        $this->run();
+    public function __construct() {
+        parent::__construct();
     }
+
 }
 
 
