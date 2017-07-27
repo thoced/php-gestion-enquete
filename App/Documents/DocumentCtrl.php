4bbf5e864a2d3f862e5775580d0b08f1f85340c1
Thonon <Thonon@Thonon-PC.lan>
2017-07-27 10:41:32 +0200

    - Implémentation du code du DocumentCtrl

--- App/Documents/DocumentCtrl.php
+++ App/Documents/DocumentCtrl.php
@@ -15,13 +15,51 @@ require_once './App/Controller/BaseController.php';
 
 class DocumentCtrl extends BaseController{
     //put your code here
-    public function run($login, $setting, $action, $id, $update) {
-        
-    }
+   
     
     public function __construct() {
         parent::__construct();
     }
 
+    public function delete($login, $setting, $action, $id, $update) {
+        $db = DbConnect::getInstance();
+        $req = $db->_dbb->prepare('delete from t_document where id = :id ');
+        $req->execute(array("id" => $id));
+        // rappel à la methode show
+        $this->show($login, $setting, $action, $id, $update);
+    }
+
+    public function insert($login, $setting, $action, $id, $update) {
+        
+    }
+
+    public function select($login, $setting, $action, $id, $update) {
+        
+    }
+
+    public function show($login, $setting, $action, $id, $update) {
+        $db = DbConnect::getInstance();
+        $req = $db->_dbb->prepare('select * from t_document inner join t_type_document ON t_document.ref_id_type = t_type_document.id where t_document.ref_id_folders = :idfolder');
+        $req->execute(array("idfolder" => $setting->getIdFolderSelected()));
+        // récupération des types de document
+        $db = DbConnect::getInstance();
+        $reqType = $db->_dbb->prepare('select * from t_type_document');
+        $reqType->execute();
+        // appel à la vue
+        require './App/Documents/DocumentView.php';
+    }
+
+    public function update($login, $setting, $action, $id, $update) {
+        $db = DbConnect::getInstance();
+        $req = $db->_dbb->prepare("update t_document set ref_id_type = :ref_id_type, titre = :titre, commentaire = :commentaire, date = :date, reference = :reference where id = :id");
+        $req->execute(array("ref_id_type" => $update['type'],
+                            "titre" => $update['titre'],
+                            "commentaire" => $update['commentaire'],
+                            "date" => $update['date'],
+                            "reference" => $update['reference'],
+                            "id" => $id));
+        // appel à la vue
+        $this->show($login, $setting, $action, $id, $update);
+    }
 
 }
