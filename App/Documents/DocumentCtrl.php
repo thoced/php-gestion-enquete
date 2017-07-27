bb31c40110631accaefebaac937bb4bacf872830
Thonon <Thonon@Thonon-PC.lan>
2017-07-27 08:34:11 +0200

    - Creation du DocumentCtrl

---
+++ App/Documents/DocumentCtrl.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DocumentCtrl
+ *
+ * @author Thonon
+ */
+require_once './App/Controller/BaseController.php';
+
+class DocumentCtrl extends BaseController{
+    //put your code here
+    public function run($login, $setting, $action, $id, $update) {
+        
+    }
+    
+    public function __construct() {
+        parent::__construct();
+    }
+
+
+}
