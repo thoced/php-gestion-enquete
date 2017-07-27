f67c5d03ae63a4cfc44ac28605f8623f8362223d
Thonon <Thonon@Thonon-PC.lan>
2017-07-27 08:54:21 +0200

    - Création des méthodes abstraites select,update,delete,insert,show dans la classe de base BaseController et gestion catch (pompe à action) migré dans le constructeur de la classe de base

--- App/Controller/BaseController.php
+++ App/Controller/BaseController.php
@@ -13,7 +13,17 @@
  */
 abstract class BaseController 
 {
-    abstract public function run($login, $setting,$action,$id,$update);
+   // abstract public function run($login, $setting,$action,$id,$update);
+    
+    abstract public function show($login,$setting,$action,$id,$update);
+    
+    abstract public function update($login,$setting,$action,$id,$update);
+    
+    abstract public function delete($login,$setting,$action,$id,$update);
+    
+    abstract public function insert($login,$setting,$action,$id,$update);
+    
+    abstract public function select($login,$setting,$action,$id,$update);
     
     public function __construct() 
     {
@@ -44,7 +54,22 @@ abstract class BaseController
             $update = $_POST;
         }
         
+        switch($action)
+        {
+            case 'DELETE':  $this->delete($login,$setting,$action,$id,$update);
+                            break;
+            case 'UPDATE':  $this->update($login,$setting,$action,$id,$update);
+                            break;
+            case 'INSERT':  $this->insert($login,$setting,$action,$id,$update);
+                            break;
+            case "SELECT":  $this->select($login, $setting, $action, $id, $update);
+                            break;
+            default      :  $this->show($login,$setting,$action,$id,$update);
+                            break;
+        }
         // appel au methode run des enfants
-        $this->run($login,$setting,$action,$id,$update);
+       // $this->run($login,$setting,$action,$id,$update);
     }
+    
+    
 }
