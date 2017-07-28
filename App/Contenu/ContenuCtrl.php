4c243481c1d87ddb099c9b74a4354934cf9cce4f
Thonon <Thonon@Thonon-PC.lan>
2017-07-28 16:49:30 +0200

    - Creation de la classe ContenuCtrl qui gère l'insertion des contenu text des documents

---
+++ App/Contenu/ContenuCtrl.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ContenuCtrl
+ *
+ * @author Thonon
+ */
+require_once './App/Controller/BaseController.php';
+
+class ContenuCtrl extends BaseController{
+    //put your code here
+    public function delete($login, $setting, $action, $id, $update) {
+        $this->show($login, $setting, $action, $id, $update);
+    }
+
+    public function insert($login, $setting, $action, $id, $update) {
+        $this->show($login, $setting, $action, $id, $update);
+    }
+
+    public function select($login, $setting, $action, $id, $update) {
+        $this->show($login, $setting, $action, $id, $update);
+    }
+
+    public function show($login, $setting, $action, $id, $update) {
+        
+        $db = DbConnect::getInstance();
+        $req = $db->_dbb->prepare('select contenu from t_document where id = :id');
+        $req->execute(array("id" => $id));
+        $row = $req->fetch();
+        
+        $contenu = $row['contenu'];
+        // appel à la vue
+        require './App/Contenu/ContenuView.php';
+    }
+
+    public function update($login, $setting, $action, $id, $update) {
+        $db = DbConnect::getInstance();
+        $req = $db->_dbb->prepare('update t_document set contenu = :contenu where id = :id');
+        $req->execute(array("contenu" => $update['contenu'],
+                            "id" => $id));
+        
+        $this->show($login, $setting, $action, $id, $update);
+    }
+
+}
