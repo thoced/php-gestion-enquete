6fa5457e7ac464a9c3179398dbd7b51078c09b5c
Thonon <Thonon@Thonon-PC.lan>
2017-07-28 10:59:54 +0200

    - Geston Exception

--- App/Folder/FolderCtrl.php
+++ App/Folder/FolderCtrl.php
@@ -19,13 +19,19 @@ class FolderCtrl extends BaseController{
     
     public function show($login,$setting,$action,$id,$update)
     {
+        if(!isset($login)){
+            throw new Exception("La variable login est vide, une erreur est survenue");
+        }
+        
         $db = DbConnect::getInstance();
         $req = $db->_dbb->prepare("select * from t_folders inner join t_link_group_folders on t_folders.id = t_link_group_folders.ref_id_folders "
                     . "where t_link_group_folders.ref_id_group like ("
                     . "select DISTINCT t_group.id from t_group inner join t_link_group_users on t_group.id = t_link_group_users.ref_id_group "
                     . "where t_link_group_users.ref_id_users = (select t_users.id from t_users where login = :login)) AND t_folders.visible = TRUE");
         
-        $req->execute(array("login" => $login->login));
+        if($req->execute(array("login" => $login->login)) == false){
+            throw new Exception("Erreur dans la selection des dossiers, une erreur est survenue");
+        }
         
         // appel à la vue
         require './App/Folder/ViewFoldersView.php';
@@ -39,6 +45,8 @@ class FolderCtrl extends BaseController{
             header('location: .?target_link=MAINVIEW');
             
         }
+        else
+            throw new Exception("La variable id n'est pas numérique");
     }
     
     public function __construct() {
