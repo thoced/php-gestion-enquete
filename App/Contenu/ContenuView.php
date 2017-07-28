d6ea4a945b30d0f02b5d41c9ba710c351e5e889c
Thonon <Thonon@Thonon-PC.lan>
2017-07-28 16:49:50 +0200

    - Creation de la vue ContenuView qui gère l'affichage des contenu des documents

---
+++ App/Contenu/ContenuView.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./Style/.AppCss.css"> 
+        <title></title>
+    </head>
+    <body>
+         <form name="form_contenu" action="<?php echo '?target_link=VIEWCONTENU&action=UPDATE&id=' . $id; ?>" method="POST">
+        <div class="contenuArea">
+            <p style="text-align: center;">
+            <textarea  name="contenu" cols="128" rows="64"><?php echo $contenu;?></textarea>
+            </p>
+        </div>
+         <p style="text-align: center;"><input type="submit" value="Enregistrer"></p>
+         </form>
+        <a class="return" href="?target_link=VIEWDOCUMENTS">Retour</a>
+    </body>
+</html>
