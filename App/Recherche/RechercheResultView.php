5911465892974302d22928affb5e12b3e14dd514
Thonon <Thonon@Thonon-PC.lan>
2017-08-06 14:17:00 +0200

    - Création de la vue d'affichage des résultats des recherches faites sur base du contenu

---
+++ App/Recherche/RechercheResultView.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+         <link rel="stylesheet" href="./Style/.AppCss.css">
+        <title></title>
+    </head>
+    <body>
+        
+        <div class="list">
+            <table style="width: 100%;">
+            <?php
+            $i = 1;
+            while($row = $req->fetch()){
+       echo "<tr><td>" . $i . "</td>"
+               . "<td>" . $row['type'] . "</td>" 
+    . "<td>" . $row['titre'] . '</td>'
+               . '<td>' . $row['commentaire'] . '</td>'
+               . '<td>' . $row['date'] . '</td>'
+               . '<td>' . $row['reference'] . '</td>'
+               . '<td>' . '<a href="?target_link=VIEWSELECTANNEXES&id=' . $row[0] . '">Annexes</a>' . '</td>'
+               . '<td>' . '<a class="contenu" href="?target_link=VIEWCONTENU&id=' . $row['0'] . '">Contenu</a>'.  '</td>'
+               . '</tr>';
+       $i++;
+            }
+           
+            ?>
+                
+            </table>
+        </div>
+         <a class="return" href="?target_link=VIEWRECHERCHES">Retour</a>
+    </body>
+</html>
