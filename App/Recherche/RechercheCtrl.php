b07a88b5593dfd1c473127ec8c6d7ff3552953a5
Thonon <Thonon@Thonon-PC.lan>
2017-08-06 14:16:34 +0200

    - implémentation de la méthode select pour effectuer une recherche sur le contenu dans la classe RechercheCtrl

--- App/Recherche/RechercheCtrl.php
+++ App/Recherche/RechercheCtrl.php
@@ -17,6 +17,7 @@ require_once './App/Controller/BaseController.php';
 
 use App\DbConnect;
 use App\Controller\BaseController;
+use App\Exception\ExceptionCtrl;
 
 class RechercheCtrl extends BaseController{
     //put your code here
@@ -30,8 +31,24 @@ class RechercheCtrl extends BaseController{
 
     public function select($login, $setting, $action, $id, $update) {
         
+        if(!isset($setting))
+            throw new Exception("Probleme avec la valeur setting, une erreur est survenue");
         
-        $this->show($login, $setting, $action, $id, $update);
+        if(!isset($update) || !isset($update['contenu']))
+            throw new Exception("Probleme avec les valeurs de la variable update, une erreur est survenue");
+        
+        // recherche dans les documents liés au folder en cours
+        $db = DbConnect::getInstance();
+        $contenu = "%" . trim($update['contenu']) . "%";
+        $req = $db->_dbb->prepare("select * from t_document inner join t_type_document ON t_document.ref_id_type = t_type_document.id where ref_id_folders = :ref_id_folders AND contenu like :contenu");
+        if($req->execute(array("ref_id_folders" => $setting->getIdFolderSelected(),
+                               "contenu" => $contenu)) == false)
+            throw new \Exception("Une erreur est survenue dans la recherche");
+        
+     
+        
+        // appel à la vue
+        require './App/Recherche/RechercheResultView.php';
         
     }
 
