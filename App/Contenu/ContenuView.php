6ca4367f6e8a051fe8f84e6305df1b7df9c862c4
Thonon <Thonon@Thonon-PC.lan>
2017-08-06 20:51:21 +0200

    - Ajout du titre

--- App/Contenu/ContenuView.php
+++ App/Contenu/ContenuView.php
@@ -5,10 +5,11 @@
         <title></title>
     </head>
     <body>
+         <h1 style="text-align: center;">Vue sur le contenu</h1>
          <form name="form_contenu" action="<?php echo '?target_link=VIEWCONTENU&action=UPDATE&id=' . $id; ?>" method="POST">
         <div class="contenuArea">
             <p style="text-align: center;">
-            <textarea  name="contenu" cols="128" rows="64"><?php echo $contenu;?></textarea>
+            <textarea  name="contenu" cols="128" rows="48"><?php echo $contenu;?></textarea>
             </p>
         </div>
          <p style="text-align: center;"><input type="submit" value="Enregistrer"></p>
